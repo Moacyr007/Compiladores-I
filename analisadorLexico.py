8d6a74e037b98f0f0f3ba0a270d5e35abb2f7087
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2020-02-16 11:53:58 -0400

    corrigido analisador lexico

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -6,43 +6,56 @@ path = r'D:\Projetos\analisadorLexicoSecreto\entradaAva.txt'
 f = open(path, 'r')
 entrada = f.read()
 
-palavrasReservadas= ['var','id','integer','real','if', 'then']
-simbolos = [':',',' , ';' , ':=','+']
-#operadores=['+','/','*','-']
-#operadoresRelacionais=['<', '<=', '==', '!=', '>=', '>']
 
+diconario = {
+  "var" : "declaracao",
+  "integer": "tipo",
+  "real": "tipo",
+  "if": "condicional",
+  "then": "acao condicional",
+  "+":"soma",
+  ":": "dois pontos",
+  ",": "virgula",
+  ";": "pronto e virgula",
+  ":=": "atribuicao"
+}
+simbolos = [':',',' , ';' , ':=','+']
 lentras = 'abcdefghijklmnopqrstuvwxyz'
 numeros = '0123456789'
-
-entrada = entrada.replace(' ', '')
-entrada = entrada.replace('\n', '')
+espacoEQuebraDeLinha= [' ', '\n']
 
 tamanhoEntrada= len(entrada)
-
+finalEntrada = tamanhoEntrada-1
 tokens = []
 a = 0
 while a < tamanhoEntrada-1:
-    print('a:',a)
-    #identificadores
-    token = ''
-    while entrada[a] in numeros or entrada[a] in lentras and a < tamanhoEntrada-1:
-        token += entrada[a]
-        a += 1
-    if a == tamanhoEntrada-1:
-        token += entrada[a]
-    if(token in palavrasReservadas):
-        tokens.append([token,'palavra reservada'])
-    else:
-        tokens.append([token,'identificador'])
+    
     if entrada[a] == ':' and entrada[a+1] == '=':
         token = ':='
-        tokens.append([token,'atribuicao'])
+        tokens.append([token, diconario[token]])
         a+=2
-    if entrada[a] in simbolos:
-        a+=1
+    token = ''
+    
+    while entrada[a] in numeros or entrada[a] in lentras and a < finalEntrada:
+        token += entrada[a]
+        a += 1
+    if a == finalEntrada:
+        token += entrada[a]
+    if(token in diconario):
+        tokens.append([token, diconario[token]])
+    elif(token != ''):
+        tokens.append([token, "identificador"])
+    
+    if(entrada[a] in diconario):
         token = entrada[a]
-        tokens.append([token,'simbolo'])
+        tokens.append([token, diconario[token]])
+        a += 1
 
-print(tokens)
-  
+    if entrada[a]in espacoEQuebraDeLinha and a < finalEntrada:
+        a+=1
+    if (entrada[a] not in simbolos and entrada[a] not in lentras  and entrada[a] not in numeros) and entrada[a] not in espacoEQuebraDeLinha:
+        raise ValueError('Erro em:', entrada[a])
 
+
+for x in tokens:
+    print(x[0], "|", x[1] )
